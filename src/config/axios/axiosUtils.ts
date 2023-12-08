4ca21aa952ac085b6daada0aa236ee9725e15764
Linggos06 <julia.snk@hotmail.com>
2023-12-08 03:27:12 +0300

    Create fetch function

--- src/config/axios/axiosUtils.ts
+++ src/config/axios/axiosUtils.ts
@@ -1,6 +1,23 @@
 import instance from './axiosConfig'
-import { AxiosResponse } from 'axios'
+import { AxiosError, AxiosResponse } from 'axios'
 
 const apiGet = async <T>(url: string): Promise<AxiosResponse<T>> => instance.get<T>(url)
 
-export { apiGet }
+const fetchData = async (
+  url: string,
+  successHandler: (arg0: AxiosResponse<any, any>) => any,
+  errorHandler: (arg0: AxiosError | unknown) => unknown
+) => {
+  try {
+    const response: AxiosResponse<any, any> = await apiGet(url)
+    successHandler(response.data)
+    return response.data
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      const { error } = err?.response?.data
+      errorHandler(error)
+    }
+  }
+}
+
+export { apiGet, fetchData }
